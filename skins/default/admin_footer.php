ea38fceacc7edddd8c4b0d3b34a324e274a79577
besenok1 <besenok@antarctida.com>
2016-07-14 12:47:07 +0200

    Update admin_footer.php

--- skins/default/admin_footer.php
+++ skins/default/admin_footer.php
@@ -8,13 +8,13 @@
 ?>
 
 <table width="100%" border="0" cellspacing="8" cellpadding="0" id="footlinks">
-			  <tr>
-				<td><a href="admin_main.php">Main</a> | <a href="chess_tournament_v2.php">Tournament Setup</a> 
-                  | <a href="create_newsletter.php">Create NewsLetter</a> | <a href="create_activity.php">Create Activity</a> | 
-                  <a href="manage_players.php">Players</a> | <a href="manage_billing_data.php">Player Billing</a> | <a href="manage_news_data.php">News/Msc Data</a> 
-                  | <a href="manage_lookandfeel.php">Server Management</a><?php if($_SESSION['UNAME'] != ""){ ?>| <a href="admin_logout.php">Logout</a><?php }else{ ?>| <a href="index.php">Login</a><?php } ?></td>
-			  </tr>
-			  <tr>
-				<td>&nbsp;</td>
-			  </tr>
-			</table>
+	<tr>
+		<td><a href="admin_main.php"><?php echo GetStringFromStringTable("IDS_APPLICATION_HEADERS_1", $config);?></a> | <a href="chess_tournament_v2.php"><?php echo GetStringFromStringTable("IDS_APPLICATION_HEADERS_2", $config);?></a> 
+                  | <a href="create_newsletter.php"><?php echo GetStringFromStringTable("IDS_APPLICATION_HEADERS_3", $config);?></a> | <a href="create_activity.php"><?php echo GetStringFromStringTable("IDS_APPLICATION_HEADERS_17", $config);?></a> | 
+                  <a href="manage_players.php"><?php echo GetStringFromStringTable("IDS_APPLICATION_HEADERS_4", $config);?></a> | <a href="manage_billing_data.php"><?php echo GetStringFromStringTable("IDS_APPLICATION_HEADERS_5", $config);?></a> | <a href="manage_news_data.php"><?php echo GetStringFromStringTable("IDS_APPLICATION_HEADERS_6", $config);?></a> 
+                  | <a href="manage_lookandfeel.php"><?php echo GetStringFromStringTable("IDS_APPLICATION_HEADERS_7", $config);?></a><?php if($_SESSION['UNAME'] != ""){ ?>| <a href="admin_logout.php"><?php echo GetStringFromStringTable("IDS_APPLICATION_HEADERS_8", $config);?></a><?php }else{ ?>| <a href="index.php"><?php echo GetStringFromStringTable("IDS_APPLICATION_HEADERS_9", $config);?></a><?php } ?></td>
+	</tr>
+	<tr>
+		<td>&nbsp;</td>
+	</tr>
+</table>
