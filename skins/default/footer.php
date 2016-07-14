d0322a05311d4cc620d6df9ee84708da9f86503d
besenok1 <besenok@antarctida.com>
2016-07-14 12:49:56 +0200

    Update footer.php

--- skins/default/footer.php
+++ skins/default/footer.php
@@ -8,13 +8,13 @@
 ?>
 
 <table width="100%" border="0" cellspacing="8" cellpadding="0" id="footlinks">
-			  <tr>
-				<td><a href="<?php echo $Root_Path;?>chess_members.php">Player Home</a> | <a href="<?php echo $Root_Path;?>chess_msg_center.php">Message Center</a> 
-                  | <a href="<?php echo $Root_Path;?>chess_view_games.php">View Games</a> | <a href="<?php echo $Root_Path;?>chess_find_player.php">Find Player</a> | 
-                  <a href="<?php echo $Root_Path;?>chess_buddy_list.php">Buddy List</a> | <a href="<?php echo $Root_Path;?>chess_statistics.php">Statistics</a> | <a href="<?php echo $Root_Path;?>chess_cfg.php">Configuration</a> 
-                  | <a href="<?php echo $Root_Path;?>chess_login.php">Login</a></td>
-			  </tr>
-			  <tr>
-				<td>&nbsp;</td>
-			  </tr>
-			</table>
+	<tr>
+		<td><a href="<?php echo $Root_Path;?>chess_members.php"><?php echo GetStringFromStringTable("IDS_APPLICATION_HEADERS_10", $config);?></a> | <a href="<?php echo $Root_Path;?>chess_msg_center.php"><?php echo GetStringFromStringTable("IDS_APPLICATION_HEADERS_11", $config);?></a> 
+                  | <a href="<?php echo $Root_Path;?>chess_view_games.php"><?php echo GetStringFromStringTable("IDS_APPLICATION_HEADERS_12", $config);?></a> | <a href="<?php echo $Root_Path;?>chess_find_player.php"><?php echo GetStringFromStringTable("IDS_APPLICATION_HEADERS_13", $config);?></a> | 
+                  <a href="<?php echo $Root_Path;?>chess_buddy_list.php"><?php echo GetStringFromStringTable("IDS_APPLICATION_HEADERS_14", $config);?></a> | <a href="<?php echo $Root_Path;?>chess_statistics.php"><?php echo GetStringFromStringTable("IDS_APPLICATION_HEADERS_15", $config);?></a> | <a href="<?php echo $Root_Path;?>chess_cfg.php"><?php echo GetStringFromStringTable("IDS_APPLICATION_HEADERS_16", $config);?></a> 
+                  | <a href="<?php echo $Root_Path;?>chess_login.php"><?php echo GetStringFromStringTable("IDS_MENU_TXT_20", $config);?></a></td>
+	</tr>
+	<tr>
+		<td>&nbsp;</td>
+	</tr>
+</table>
